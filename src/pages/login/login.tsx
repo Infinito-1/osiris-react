21350ee6977ffc5c89286e3bb1b7c30619c6bf05
FATEC ZONA LESTE <larissateixeira998@gmail.com>
2025-11-05 09:16:41 -0300

    altera nomes

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,61 +3,61 @@ import React from 'react'
 function login() {
   return (
     <>
-      <section class="login-section">
+      <section className="login-section">
         <h1>Osíris</h1>
         <p>Acesse sua conta ou crie uma nova</p>
 
-        <div class="tab-buttons">
-          <button id="tab-login" class="active">Entrar</button>
-          <button id="tab-register" class="inactive">Cadastrar</button>
+        <div className="tab-buttons">
+          <button id="tab-login" className="active">Entrar</button>
+          <button id="tab-register" className="inactive">Cadastrar</button>
         </div>
 
-        <div class="login-box" id="loginBox">
-          <div class="login-header">
+        <div className="login-box" id="loginBox">
+          <div className="login-header">
             <img src="../../assets/img/login/arrowin.png" alt="Ícone de login" />
             <h2>Fazer Login</h2>
           </div>
 
-          <div class="form-group">
+          <div className="form-group">
             <label for="email">E-mail</label>
             <input type="email" id="email" />
           </div>
 
-          <div class="form-group">
+          <div className="form-group">
             <label for="senha">Senha</label>
             <input type="password" id="senha" />
           </div>
 
-          <button class="login-button">Entrar</button>
-          <div class="forgot-password">
+          <button className="login-button">Entrar</button>
+          <div className="forgot-password">
             <a href="esqueceusenha.html" id="esqueceu-senha"
               >Esqueceu sua senha?</a>
           </div>
         </div>
 
-        <div class="register-box" id="registerBox">
-          <div class="login-header">
+        <div className="register-box" id="registerBox">
+          <div className="login-header">
             <img src="../../assets/img/login/PersonPlus.png" alt="Ícone de cadastro" />
             <h2>Criar Conta</h2>
           </div>
 
           <a href="./forms/empreendedorform.html">
-            <button class="register-button empreendedor" id="btnEmpreendedor">
+            <button className="register-button empreendedor" id="btnEmpreendedor">
               Empreendedor
             </button>
           </a>
           <a href="./forms/estudantesform.html">
-            <button class="register-button estudante" id="btnEstudante">
+            <button className="register-button estudante" id="btnEstudante">
               Estudante
             </button>
           </a>
           <a href="./forms/docenteform.html">
-            <button class="register-button docente" id="btnDocente">
+            <button className="register-button docente" id="btnDocente">
               Docente
             </button>
           </a>
           <a href="./forms/coordenadorform.html">
-            <button class="register-button coordenador" id="btnCoordenador">
+            <button className="register-button coordenador" id="btnCoordenador">
               Coordenador
             </button>
           </a>
