50fc32f7c4c16f43228e7a492bf5e5ee7584e1e7
FATEC ZONA LESTE <larissateixeira998@gmail.com>
2025-11-05 09:08:46 -0300

    fix: altera nome da função que recebe classes

--- src/pages/login/redefinirsenha.tsx
+++ src/pages/login/redefinirsenha.tsx
@@ -3,29 +3,29 @@ import React from 'react'
 function redefinirsenha() {
   return (
     <>
-      <section class="login-section">
+      <section className="login-section">
         <h1>Osíris</h1>
         <p>Acesse sua conta ou crie uma nova</p>
 
         {/* <!--Redefinir Senha--> */}
-        <div class="login-box" id="loginBox">
-          <div class="login-header">
+        <div className="login-box" id="loginBox">
+          <div className="login-header">
             <img src="../../assets/img/login/arrowin.png" alt="Ícone de login" />
             <h2>Redefinição de Senha</h2>
           </div>
 
-          <div class="form-group">
+          <div className="form-group">
             <label for="email">Nova Senha:</label>
             <input type="email" id="email" />
           </div>
 
-          <div class="form-group">
+          <div className="form-group">
             <label for="senha">Confirme a Senha:</label>
             <input type="password" id="senha" />
           </div>
 
-          <a href="login.html"><button class="login-button" id="reset-button">Redefinir</button></a>
-          <div class="forgot-password">
+          <a href="login.html"><button className="login-button" id="reset-button">Redefinir</button></a>
+          <div className="forgot-password">
               <a href="#" id="suporte-link">Problemas? Fale com nosso suporte aqui</a>
           </div>
         </div>
