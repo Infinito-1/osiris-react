a4d7899a1cf4908de013c3dc6ce464165888d766
unknown <rafaelaolbo@gmail.com>
2025-11-15 10:09:03 -0300

    finalização do perfil de grupo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/home/Home";
 import Grupos from "./pages/grupo/Grupos";
+import SobreGrupo from "./pages/grupo/Perfil_grupo";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/grupos" element={<Grupos />} />
+            <Route path="/perfil_grupo" element={<SobreGrupo />} />
           </Routes>
 
         </div>
