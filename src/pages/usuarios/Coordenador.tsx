a2a67d5c6d6b1472e9daac5b989f5060b80cbc72
Guilherme Gervasio <guilherme.silva0509@outlook.com>
2025-11-19 12:40:01 -0300

    arrumando formatação do tab de navegação do coordenador

--- src/pages/usuarios/Coordenador.tsx
+++ src/pages/usuarios/Coordenador.tsx
@@ -136,12 +136,13 @@ export default function DashboardCoordenador() {
           Gerencie demandas e acompanhe projetos do curso
         </p>
 
-        <div className="lg:col-span-2">
+        {/* <div className="lg:col-span-2">
           <TabsNavegacao />
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-4">
           <div className="lg:col-span-2 space-y-6">
+            <TabsNavegacao />
             <CardDemanda />
             <CardDemanda />
             <CardDemanda />
