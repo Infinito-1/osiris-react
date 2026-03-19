dc2eb229df9917210c3186c5d7e78a1b5006b637
Guilherme Gervásio <guilherme.silva0509@outlook.com>
2026-03-19 20:31:02 -0300

    Padronizando botões da tela inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -147,16 +147,15 @@ function Home() {
             <h1 className="text-5xl justify-center font-semibold text-[#DAD4C8] w-[70%] mx-auto text-center">Conectamos <span className="text-[#A33E38]">Empreendedores</span> com <span className="text-[#A33E38]">Estudantes de Tecnologia</span></h1>
             <p className="text-[#DAD4C8] text-center py-5 text-2xl w-[80%] mx-auto">Plataforma que une demandas reais de negócios com projetos acadêmicos da Fatec Zona Leste, criando oportunidades de aprendizado prático e soluções inovadoras.</p>
 
-            <div className="flex gap-5 justify-center">
-                <button className="text-white text-2x1 flex gap-1 bg-[#5F747F] rounded-2xl p-3 items-center cursor-pointer transition-opacity duration-300 hover:opacity-70">
-                    <img className="size-6" src={foguete}/>
-                    <p>Cadastrar Projeto</p>
-                </button>
-
-                <button className="bg-[#F1F7EE] text-2x1 flex gap-1 rounded-2xl p-3 items-center cursor-pointer transition-opacity duration-300 hover:opacity-70">
-                    <img className="size-4" src={pessoas}/>
-                    <p className="text-[#021926]">Formar Equipe </p>
-                </button>
+            <div className="flex space-x-4 mt-2 justify-center">
+                        <button className="flex items-center space-x-2 bg-[#5F747F] text-white py-3 px-5 rounded-lg text-base font-medium transition-colors duration-200 hover:bg-[#556872]">
+                            <img src={foguete} alt="Cadastrar Projeto" className="w-5 h-5" />
+                            <p>Cadastrar Projeto</p>
+                        </button>
+                        <button className="flex items-center space-x-2 bg-[#F1F7EE] text-[#000000] py-3 px-5 rounded-lg text-base font-medium transition-colors duration-200 hover:bg-[#c4c9c2]">
+                            <img src={pessoas} alt="Formar Equipe" className="w-5 h-5" />
+                            <p>Formar Equipe</p>
+                        </button>
             </div>
         </section>
 
